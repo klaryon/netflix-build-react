cf1e6e2305a1653152074977e4f91946abb6e2df
klaryon <klary.ardon@gmail.com>
2021-03-18 15:05:09 +0100

    user authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyCVHVI0Gtufq-RNCv2XZ-9MI7pDu7gb3ZA",
     authDomain: "netflix-build-react-5fcb3.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "910925868190",
     appId: "1:910925868190:web:70e3130d02635fe42b9f8f"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default db;

80dd5e5f35792bc83f9cf77b754e683ee77f5b0d
klaryon <klary.ardon@gmail.com>
2021-03-18 09:24:13 +0100

    screens router-dom added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './components/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Switch>
+          <Route path="/test">
+            <h1>What's up</h1>
+          </Route>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
